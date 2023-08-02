b88d7fd1797f72b8d98b2c56091430932804a2a1
Mohan <120453036+Mohanbalaji1998@users.noreply.github.com>
2023-08-02 11:37:40 +0530

    Update and rename aws_cost.py to aws_production_cost.py

--- aws_production_cost.py
+++ aws_production_cost.py
@@ -1,29 +1,25 @@
 import boto3
 from datetime import datetime, timedelta, date
 import json
-import psycopg2
+import mysql.connector
 
-
-mydb = psycopg2.connect(
-    host="***********",
-    database="********",
-    user="*********",
-    password="*********"
+mydb = mysql.connector.connect(
+    host="********",
+    database="*********",  # Update this to the correct MySQL database name
+    user="****",
+    password="******"
 )
 
-today= '2023-07-25'
-yesterday= '2023-07-01'
+today = '2023-08-03' #Change this date as the current date of deployment
+yesterday = '2023-07-01'
 # today = datetime.now().date()
 # yesterday = today - taimedelta(days=1)
 
-
 aws_access_key_id = '********'
-aws_secret_access_key = '********'
+aws_secret_access_key = '*********'
 client = boto3.client('ce',
-                      #   region_name=region_name,
                       aws_access_key_id=aws_access_key_id,
                       aws_secret_access_key=aws_secret_access_key)
-
 
 response = client.get_cost_and_usage(
     TimePeriod={
@@ -85,18 +81,19 @@ def checkService(service):
         return 'tax'
 
 
+
 def sort(response):
     requestId = response['ResponseMetadata']['RequestId']
     # Modifying based on datetime format
     currTime = datetime.now()
     dt_string = currTime.strftime("%Y-%m-%d %H:%M:%S")
+
     for j in range(len(response['ResultsByTime'])):
-        # Time duration
         start_date = response['ResultsByTime'][j]['TimePeriod']['Start']
         end_date = response['ResultsByTime'][j]['TimePeriod']['End']
 
         # Development environment dictionary
-        productionCost = {
+        devCost = {
             "request_Id": requestId,
             "start_date": start_date,
             "end_date": end_date,
@@ -129,26 +126,34 @@ def sort(response):
             # getting the environment
             environment = i['Keys'][1]
 
-            # checking for production environment
-            if environment == 'Environment$production':
+            # checking for dev environment
+            if environment == 'Environment$staging':
                 ser = checkService(service)
 
-                productionCost[ser] += float(i['Metrics']['BlendedCost']['Amount'])
-    
+                devCost[ser] += float(i['Metrics']['BlendedCost']['Amount'])
+
+
         cursor = mydb.cursor()
-        insert = 'INSERT INTO aws_production_cost (request_id,start_date,end_date,request_time,environment,aws_key_management_service,aws_lambda,aws_secrets_manager,amazon_api_gateway,amazon_cloudfront,amazon_ec2_container_registry_ecr,ec2_other,amazon_elastic_compute_cloud_compute,amazon_elastic_container_service,amazon_elastic_container_service_for_kubernetes,amazon_elastic_load_balancer,amazon_relational_database_service,amazon_simple_email_service,amazon_simple_notification_service,amazon_simple_storage_service,amazon_cloudwatch,aws_cost_explorer,tax) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
-        insert_production_value = (
-        productionCost['request_Id'], productionCost['start_date'], productionCost['end_date'], productionCost['dateTime'], productionCost['environment'],
-        productionCost['awsKeyManagementService'], productionCost['awsLambda'], productionCost['awsSecretsManager'], productionCost['amazonAPIGateway'],
-        productionCost['amazonCloudFront'], productionCost['amazonEC2ContainerRegistry'], productionCost['ec2_Other'],
-        productionCost['amazonElasticComputeCloud_Compute'], productionCost['amazonElasticContainerService'],
-        productionCost['amazonElasticContainerServiceforKubernetes'], productionCost['amazonElasticLoadBalancing'],
-        productionCost['amazonRelationalDatabaseService'], productionCost['amazonSimpleEmailService'],
-        productionCost['amazonSimpleNotificationService'], productionCost['amazonSimpleStorageService'], productionCost['amazonCloudWatch'],
-        productionCost['awsCostExplorer'], productionCost['tax'])
-        cursor.execute(insert, (insert_production_value))
-        
+
+        insert = 'INSERT INTO aws_production_cost (request_id, start_date, end_date, request_time, environment, aws_key_management_service, aws_lambda, aws_secrets_manager, amazon_api_gateway, amazon_cloudfront, amazon_ec2_container_registry_ecr, ec2_other, amazon_elastic_compute_cloud_compute, amazon_elastic_container_service, amazon_elastic_container_service_for_kubernetes, amazon_elastic_load_balancer, amazon_relational_database_service, amazon_simple_email_service, amazon_simple_notification_service, amazon_simple_storage_service, amazon_cloudwatch, aws_cost_explorer, tax) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+
+        insert_dev_value = (
+            devCost['request_Id'], devCost['start_date'], devCost['end_date'], devCost['dateTime'],
+            devCost['environment'],
+            devCost['awsKeyManagementService'], devCost['awsLambda'], devCost['awsSecretsManager'],
+            devCost['amazonAPIGateway'],
+            devCost['amazonCloudFront'], devCost['amazonEC2ContainerRegistry'], devCost['ec2_Other'],
+            devCost['amazonElasticComputeCloud_Compute'], devCost['amazonElasticContainerService'],
+            devCost['amazonElasticContainerServiceforKubernetes'], devCost['amazonElasticLoadBalancing'],
+            devCost['amazonRelationalDatabaseService'], devCost['amazonSimpleEmailService'],
+            devCost['amazonSimpleNotificationService'], devCost['amazonSimpleStorageService'],
+            devCost['amazonCloudWatch'],
+            devCost['awsCostExplorer'], devCost['tax'])
+
+        # Execute the query for MySQL
+        cursor.execute(insert, insert_dev_value)
         mydb.commit()
-        print(f"Records Inserted in table")
+
+        print("Records Inserted in table")
 
 sort(response)
